66f7b2ba5f85570885127957b98d262d0f532fd4
Mikey Beck <mike@mikeybeck.com>
2015-12-03 10:23:36 +1300

    Updating to version 1.7

--- options.php
+++ options.php
@@ -4,16 +4,22 @@
      */
     $string_locate_themes = wp_get_themes();
     $string_locate_plugins = get_plugins();
+
+	$this_url = admin_url( ( is_multisite() ? 'network/admin.php' : 'tools.php' ) . '?page=string-locator' );
 ?>
 <div class="wrap">
     <h2>
-        <?php _e( 'String Locator', 'string-locator-plugin' ); ?>
+        <?php _e( 'String Locator', 'string-locator' ); ?>
     </h2>
 
-    <form action="" method="post">
-        <label for="string-locator-search"><?php _e( 'Search through', 'string-locator-plugin' ); ?></label>
+    <form action="<?php echo esc_url( $this_url ); ?>" method="post">
+        <label for="string-locator-search"><?php _e( 'Search through', 'string-locator' ); ?></label>
         <select name="string-locator-search" id="string-locator-search">
-            <optgroup label="<?php _e( 'Themes', 'string-locator-plugin' ); ?>">
+	        <optgroup label="<?php _e( 'Core', 'string-locator' ); ?>">
+		        <option value="core"><?php _e( 'The whole WordPress directory', 'string-locator' ); ?></option>
+	        </optgroup>
+            <optgroup label="<?php _e( 'Themes', 'string-locator' ); ?>">
+	            <option value="t--"<?php echo ( isset( $_POST['string-locator-search'] ) && 't--' == $_POST['string-locator-search'] ? ' selected="selected"' : '' ); ?>>&mdash; <?php _e( 'All themes', 'string-locator' ); ?> &mdash;</option>
                 <?php
                     /**
                      * Loop through themes for our dropdown list
@@ -28,7 +34,8 @@
                     }
                 ?>
             </optgroup>
-            <optgroup label="<?php _e( 'Plugins', 'string-locator-plugin' ); ?>">
+            <optgroup label="<?php _e( 'Plugins', 'string-locator' ); ?>">
+	            <option value="p--"<?php echo ( isset( $_POST['string-locator-search'] ) && 'p--' == $_POST['string-locator-search'] ? ' selected="selected"' : '' ); ?>>&mdash; <?php _e( 'All plugins', 'string-locator' ); ?> &mdash;</option>
                 <?php
                     /**
                      * Loop through plugins for our dropdown list
@@ -44,131 +51,112 @@
             </optgroup>
         </select>
 
-        <label for="string-locator-string"><?php _e( 'Search string', 'string-locator-plugin' ); ?></label>
+        <label for="string-locator-string"><?php _e( 'Search string', 'string-locator' ); ?></label>
         <input type="text" name="string-locator-string" id="string-locator-string" value="<?php echo ( isset( $_POST['string-locator-string'] ) ? $_POST['string-locator-string'] : '' ); ?>" />
 
-        <?php submit_button( __( 'Search', 'string-locator-plugin' ) ); ?>
+	    <p>
+	        <input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e( 'Search', 'string-locator' ); ?>">
+	        <a href="<?php echo esc_url( $this_url . '&restore=true' ); ?>" class="button button-primary"><?php _e( 'Restore last search', 'string-locator' ); ?></a>
+        </p>
     </form>
-
     <?php
-        if ( isset( $_POST['string-locator-search'] ) )
+        if ( isset( $_POST['string-locator-search'] ) || isset( $_GET['restore'] ) )
         {
-    ?>
+            $found = array();
+            $path = ABSPATH . 'wp-content/';
 
-    <form action="" method="post">
-        <table class="wp-lsit-table widefat fixed">
-            <thead>
-                <tr>
-                    <th scope="col" style=""><?php _e( 'Line', 'string-locator-plugin' ); ?></th>
-                    <th scope="col" style=""><?php _e( 'File', 'string-locator-plugin' ); ?></th>
-                    <th scope="col" style=""><?php _e( 'String', 'string-locator-plugin' ); ?></th>
-                </tr>
-            </thead>
+            $table = new String_Locator_Table();
 
-            <tfoot>
-                <tr>
-                    <th scope="col" style=""><?php _e( 'Line', 'string-locator-plugin' ); ?></th>
-                    <th scope="col" style=""><?php _e( 'File', 'string-locator-plugin' ); ?></th>
-                    <th scope="col" style=""><?php _e( 'String', 'string-locator-plugin' ); ?></th>
-                </tr>
-            </tfoot>
+            global $string_locator;
 
-            <tbody>
-            <?php
-                $found = false;
-                $path = ABSPATH . 'wp-content/';
+            $theme = false;
+            $plugin = false;
 
-                /**
-                 * Check what we are search through, a theme or a plugin
-                 */
-                if ( substr( $_POST['string-locator-search'], 0, 2 ) == 't-' )
-                {
-                    $theme = substr( $_POST['string-locator-search'], 2 );
-                    $path .= 'themes/' . $theme;
-                }
-                else {
-                    $plugin = explode( '/', substr( $_POST['string-locator-search'], 2 ) );
-                    $path .= 'plugins/' . $plugin[0];
-                }
+            if ( isset( $_GET['restore'] ) ) {
+	            $found = $string_locator->restore_scan_path();
+            }
+            else {
+                if ( 3 == strlen( $_POST['string-locator-search'] ) && '-' == substr( $_POST['string-locator-search'], 2, 1 ) ) {
+	                /**
+	                 * We are doing a search of all themes or plugins
+	                 */
+	                if ( substr( $_POST['string-locator-search'], 0, 2 ) == 't-' ) {
+		                $path .= 'themes/';
 
-                $relativepath = str_replace( ABSPATH, '', $path );
+		                foreach ( $string_locate_themes AS $string_locate_theme_slug => $string_locate_theme ) {
+			                $search = $string_locator->scan_path( $path . $string_locate_theme_slug, $_POST['string-locator-string'], 'theme', $string_locate_theme_slug );
 
-                /**
-                 * We use the PHP Iterator class to recursively check for files
-                 */
-                $paths = new RecursiveIteratorIterator(
-                    new RecursiveDirectoryIterator( $path ),
-                    RecursiveIteratorIterator::SELF_FIRST
-                );
+                            if ( $search ) {
+	                            $found = array_merge( $found, array( array(
+		                            'header' => esc_html( $string_locate_theme )
+	                            ) ) );
 
-                foreach ( $paths AS $name => $location )
-                {
-                    $linenum = 0;
-
-                    /**
-                     * If it's a directory, skip this runthrough, we can't read a directory line by line
-                     */
-                    if ( is_dir( $location->getPathname() ) )
-                        continue;
-
-                    /**
-                     * Start reading the file
-                     */
-                    $readfile = fopen( $location->getPathname(), "r" );
-                    if ( $readfile )
-                    {
-                        while ( ( $readline = fgets( $readfile ) ) !== false )
-                        {
-                            $linenum++;
-                            /**
-                             * If our string is found in this line, output the line number and other data
-                             */
-                            if ( stristr( $readline, $_POST['string-locator-string'] ) )
-                            {
-                                $found = true;
-                                echo '
-                                    <tr>
-                                        <td>' . $linenum . '</td>
-                                        <td>' . $relativepath . '/' . $location->getFilename() . '</td>
-                                        <td>' . str_ireplace( $_POST['string-locator-string'], '<strong>' . $_POST['string-locator-string'] . '</strong>', htmlentities( $readline ) ) . '</td>
-                                    </tr>
-                                ';
+                                $found = array_merge( $found, $search );
                             }
-                        }
-                    }
-                    else {
-                        /**
-                         * The file was unreadable, give the user a friendly notification
-                         */
-                        echo '
-                            <tr>
-                                <td colspan="3">
-                                    <strong>
-                                        ' . __( 'Could not read file: ', 'string-locator-plugin' ) . $location->getFilename() . '
-                                    </strong>
-                                </td>
-                            </tr>
-                        ';
+		                }
+	                } else {
+		                $path .= 'plugins/';
+
+		                foreach ( $string_locate_plugins AS $string_locate_plugin_path => $string_locate_plugin ) {
+			                $plugin = explode( '/', $string_locate_plugin_path );
+
+                            $search = $string_locator->scan_path( $path . $plugin[0], $_POST['string-locator-string'], 'plugin', $plugin[0] );
+
+                            if ( $search ) {
+	                            $found = array_merge( $found, array( array(
+		                            'header' => esc_html( $plugin[0] )
+	                            ) ) );
+
+                                $found = array_merge( $found, $search );
+                            }
+		                }
+	                }
+                } else {
+	                /**
+	                 * We are searching through an individual item
+	                 */
+
+	                /**
+	                 * Check what we are search through: WordPress core, a theme or a plugin
+	                 */
+	                if ( 'core' == $_POST['string-locator-search'] ) {
+		                $path = ABSPATH;
+		                $type = 'core';
+		                $slug = '';
+	                } elseif ( substr( $_POST['string-locator-search'], 0, 2 ) == 't-' ) {
+		                $theme = substr( $_POST['string-locator-search'], 2 );
+		                $path .= 'themes/' . $theme;
+		                $type = 'theme';
+		                $slug = $theme;
+	                } else {
+		                $plugin = explode( '/', substr( $_POST['string-locator-search'], 2 ) );
+		                $path .= 'plugins/' . $plugin[0];
+		                $type = 'plugin';
+		                $slug = $plugin[0];
+	                }
+
+                    $search = $string_locator->scan_path( $path, $_POST['string-locator-string'], $type, $slug );
+
+                    if ( $search ) {
+                        $found = array_merge( $found, $search );
                     }
                 }
+            }
 
-                /**
-                 * Give the user feedback if the string wasn't found anywhere
-                 */
-                if ( ! $found )
-                    echo '
-                        <tr>
-                            <td colspan="3">
-                                ' . __( 'Your string was not present in any of the available files.', 'string-locator-plugin' ) . '
-                            </td>
-                        </tr>
-                    ';
-            ?>
-            </tbody>
-        </table>
-    </form>
+	        $table->set_items( $found );
 
-    <?php
+	        $table->prepare_items();
+
+            /**
+             * Give the user feedback if the string was not found anywhere
+             */
+            if ( $table->has_items() ) {
+	            if ( ! isset( $_GET['restore'] ) ) {
+		            update_option( 'string-locator-results', $found, false );
+	            }
+            }
+
+	        $table->display();
         }
     ?>
 </div>
--- readme.txt
+++ readme.txt
@@ -1,22 +1,36 @@
 === String locator ===
 Contributors: Clorith
-Author URI: http://www.mrstk.net
-Plugin URI: http://www.mrstk.net/wordpress-string-locator/
+Author URI: http://www.clorith.net
+Plugin URI: http://wordpress.org/plugins/string-locator/
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6ARR8GWYHBVPN
-Tags: theme, plugin, text, search
+Tags: theme, plugin, text, search, find, editor, syntax, highlight
 Requires at least: 3.6
-Tested up to: 3.6
-Stable tag: 1.0.0
+Tested up to: 4.4
+Stable tag: 1.7
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Scan through theme and plugin files looking for text strings
+Find and edit code in your themes and plugins
 
 == Description ==
 
-When working on themes and plugins you often notice a piece of text that appears hardcoded into the files, you need to modify it, but you can't find it.
+When working on themes and plugins you often notice a piece of text that appears hardcoded into the files, you need to modify it, but you don't know where it's located in the theme files.
 
-Search through themes or plugins to identify where the string is used, showing both file names and line numbers of the matched results.
+Easily search through your themes, plugins or the WordPress core and be presented with a list of files, the matched text and what line of the file matched your search.
+You can then quickly make edits directly in your browser by clicking the link from the search results.
+
+By default a Smart-Scan is enabled when making edits, this will look for inconsistencies with braces, brackets and parenthesis that are often accidentally left.
+This drastically reduces the risk of breaking your site when making edits, but is not an absolute guarantee.
+
+As of version 1.6 the plugin will check for errors on your site after making edits, and if any are detected, will revert to the previous version of your edited file.
+
+** Translations**
+
+српски (Serbian) - Ognjen Djuraskovic
+
+Español (Spanish) - Ognjen Djuraskovic
+
+Deutsch (German) - [pixolin](http://profiles.wordpress.org/pixolin/)
 
 == Installation ==
 
@@ -26,8 +40,30 @@ Search through themes or plugins to identify where the string is used, showing b
 
 == Frequently asked questions ==
 
+= Will Smart-Scan guarantee my site is safe when making edits? =
+Although it will do it's best at detecting incorrect usage of the commonly used symbols (parenthesis, brackets and braces), there is no guarantee every possible error is detected. The best safe guard is to keep consistent backups of your site (even when not making edits).
+
+As of version 1.6, the plugin will check your site health after performing an edit. If the site is returning a site breaking error code, we'll revert to the previous version of the file.
+
+
 == Screenshots ==
+
+1. Searching through the Twenty Fourteen theme for the string 'not found'
+2. Having clicked the link for one of the results and being taken to the editor in the browser
+3. Smart-Scan has detected an inconsistency in the use of braces
 
 == Changelog ==
 
+= 1.7.0 =
+* Tested with WordPress 4.3
+* Made it uses WordPress list tables (because they look nice and I felt adventurous)
+* If the preview text is really long, an excerpt is pulled instead of making a massive text blob
+* Fixed a typo in a query argument
+* Reordered the search result list based on priority
+
+= 1.6.0 =
+* Revert edits if site health degrades as a direct cause of said edit
+
 == Upgrade notice ==
+
+4.3 compability update with other improvements, see the changelog for details
